8a2a7fe1ebaa0a3d81daa72d127757271603a261
Talha Sayed <talha@talhasayed.com>
2017-09-21 17:12:34 +0300

    Added another test

--- src/tests/Thrifty.UnitTests/TransactionServiceTests.cs
+++ src/tests/Thrifty.UnitTests/TransactionServiceTests.cs
@@ -8,15 +8,15 @@ namespace Thrifty.UnitTests
     public class TransactionServiceTests
     {
         [TestMethod]
-        public void CheckValidTransaction()
+        public void ValidateTransactionShouldReturnTrueForValidTransaction()
         {
             // Arrange
-            var transaction = new Transaction()
+            var transaction = new Transaction
             {
                 Legs = new[]
                 {
                     new TransactionLeg { IsDebit = true, Amount = 10.5M },
-                    new TransactionLeg { IsDebit = false, Amount = 10.5M },
+                    new TransactionLeg { IsDebit = false, Amount = 10.5M }
                 }
             };
 
@@ -27,6 +27,28 @@ namespace Thrifty.UnitTests
 
             //Assert
             Assert.IsTrue(result, "Method ValidateTransaction returned false for a valid transaction");
+        }
+
+        [TestMethod]
+        public void ValidateTransactionShouldReturnFalseForInvalidTransaction()
+        {
+            // Arrange
+            var transaction = new Transaction
+            {
+                Legs = new[]
+                {
+                    new TransactionLeg { IsDebit = true, Amount = 10.505M },
+                    new TransactionLeg { IsDebit = false, Amount = 10.506M }
+                }
+            };
+
+            var transactionService = new TransactionService();
+
+            // Act
+            var result = transactionService.ValidateTransaction(transaction);
+
+            //Assert
+            Assert.IsFalse(result, "Method ValidateTransaction returned true for a invalid transaction");
         }
     }
 }
