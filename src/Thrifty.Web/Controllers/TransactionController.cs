d5721fe25006d65fdcb44c0ed563d34085b69eb8
Talha Sayed <talha@talhasayed.com>
2017-10-01 23:00:11 +0300

    Add Transaction controller, AddTransactionVM and Add.cshtml view

---
+++ src/Thrifty.Web/Controllers/TransactionController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Thrifty.Abstractions;
+using Thrifty.Abstractions.Services;
+using Thrifty.Web.Models.Transaction;
+
+namespace Thrifty.Web.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly ITransactionService _transactionService;
+        private readonly IAccountService _accountService;
+
+
+        public TransactionController(ITransactionService transactionService, IAccountService accountService)
+        {
+            _transactionService = transactionService;
+            _accountService = accountService;
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            var vm = new AddTransactionVM();
+
+            vm.Accounts = await _accountService.GetAllAccounts();
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddTransactionVM vm)
+        {
+            //await _transactionService.CreateTransaction(new Transaction());
+
+            return View();
+        }
+    }
+}
---
+++ src/Thrifty.Web/Models/Transaction/AddTransactionVM.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Thrifty.Models;
+
+namespace Thrifty.Web.Models.Transaction
+{
+    public class AddTransactionVM
+    {
+        public List<Account> Accounts { get; set; }
+
+        public int DebitAccount { get; set; }
+        public decimal DebitAmount { get; set; } 
+
+        public int CreditAccount { get; set; }
+        public decimal CreditAmount { get; set; }
+    }
+}
