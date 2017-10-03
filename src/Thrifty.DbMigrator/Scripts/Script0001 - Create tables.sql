72533413e10de15ea3e1084f2c3007557cfa092a
Talha Sayed <talha@talhasayed.com>
2017-10-03 21:51:55 +0300

    Add DbUp project with a initial script to create tables

---
+++ src/Thrifty.DbMigrator/Scripts/Script0001 - Create tables.sql
@@ -0,0 +1,61 @@
+﻿/****** Object:  Table [dbo].[Account]    Script Date: 03-Oct-17 9:46:17 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Account](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CreatedOn] [datetime2](7) NOT NULL,
+	[Key] [varchar](10) NULL,
+	[Name] [nvarchar](200) NULL,
+	[UpdatedOn] [datetime2](7) NOT NULL,
+ CONSTRAINT [PK_Account] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[Transaction]    Script Date: 03-Oct-17 9:46:17 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Transaction](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [nvarchar](max) NULL,
+	[Timestamp] [datetime2](7) NOT NULL,
+ CONSTRAINT [PK_Transaction] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[TransactionLeg]    Script Date: 03-Oct-17 9:46:17 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[TransactionLeg](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Amount] [decimal](18, 2) NOT NULL,
+	[Timestamp] [datetime2](7) NOT NULL,
+	[TransactionId] [int] NOT NULL,
+ CONSTRAINT [PK_TransactionLeg] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[TransactionLeg]  WITH CHECK ADD  CONSTRAINT [FK_TransactionLeg_Transaction_TransactionId] FOREIGN KEY([TransactionId])
+REFERENCES [dbo].[Transaction] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[TransactionLeg] CHECK CONSTRAINT [FK_TransactionLeg_Transaction_TransactionId]
+GO
