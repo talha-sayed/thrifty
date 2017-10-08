ab19f0ad07b054a87ff5121b913bb1aa13564269
Talha Sayed <talha@talhasayed.com>
2017-10-08 19:39:43 +0300

    Add admin area with database controller

---
+++ src/Thrifty.Web/Areas/Admin/Controllers/DatabaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Thrifty.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("admin/[controller]")]
+    public class DatabaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
